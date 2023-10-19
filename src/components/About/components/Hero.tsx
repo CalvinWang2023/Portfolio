f7af72464ac94fe5542920e69f3264456625e5b5
Yanchun Wang <wang146@mcmaster.ca>
2023-10-18 22:47:30 -0400

    Improve responsive design of about page

--- src/components/About/components/Hero.tsx
+++ src/components/About/components/Hero.tsx
@@ -10,8 +10,8 @@ const Hero = () => {
                     <p>I'm a third year computer science student at <span>McMaster University</span>, passionate about developing <span>aesthetically pleasing</span> and <span>high-performance</span> Web Application.</p>
                     <p>I have a solid understanding of <span>React</span>, <span>Redux</span>, <span>JavaScript</span>, and <span>TypeScript</span> in front-end development;</p>
                     <p>I am actively learning <span>ASP.NET &#40;C#&#41;</span> for back-end development.</p>
-                    <p>I utilize these technologies to work on projects and continuously strive to learn and gain experience from them.</p>
-                    <p>I have a strong desire to explore different technologies and maintain an open-minded approach to learning new skills.</p>
+                    <p>I utilize these <span>technologies</span> to work on projects and continuously strive to <span>learn</span> and <span>gain</span> experience from them.</p>
+                    <p>I have a <span>strong</span> desire to explore different technologies and maintain an open-minded approach to learning <span>new skills</span>.</p>
                 </div>
                 <img src={ aboutHeroIcon } alt="about hero icon" className='aboutHeroIcon' />
             </div>
